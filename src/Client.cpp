8d07c249b182dede807c4115cce946f62cb8a80d
Logan Shy <loganshy740@gmail.com>
2020-02-19 23:06:22 -0700

    Properly implemented client firing functionality

--- src/Client.cpp
+++ src/Client.cpp
@@ -20,10 +20,25 @@
 Client::~Client() {
 }
 
+/**
+ * Helper function to clean files 
+ *@param file - input file to be opened and cleaned
+ */
+void cleanFile(string fileName){
+    ofstream file;
+    file.open(fileName, ios::out | ios::trunc);
+    if(file){
+        file << fflush;
+    } else {
+        cout << "Error : couldn't clean file: " << fileName << endl;
+    }
+    file.close();
+}
+
 
 void Client::initialize(unsigned int player, unsigned int board_size){
     this->player = player;//assign all attributes
-    this->board_name = "../../src/actionBoards/player_" + to_string(player) + ".action_board.json";
+    this->board_name = "src/actionBoards/player_" + to_string(player) + ".action_board.json";
     this->board_size = board_size;
     this->initialized = true;
     ofstream action_board;
@@ -41,7 +56,7 @@ void Client::fire(unsigned int x, unsigned int y) {
     fstream shot_file; // declare and open shot file and shot string
     string shot;
     shot = "[{\"x\":" + to_string(x) + ", \"y\":" + to_string(y) + "}]";
-    shot_file.open("../../src/shots/player_" + to_string(this->player) + ".shot.json", ios::out);
+    shot_file.open("src/shots/player_" + to_string(this->player) + ".shot.json", ios::out);
     if(shot_file){//write contents of shot string
         shot_file << shot;
     } else {
@@ -51,12 +66,18 @@ void Client::fire(unsigned int x, unsigned int y) {
 
 
 bool Client::result_available() {
-    ifstream result_file;
+    fstream result_file;
+    // result_file.open("src/results/player_"+to_string(this->player) + ".result.json", ios::in);
+    // string line; 
+    // getline(result_file, line);
+    // cout << line << endl;
+    // return false;
+
     int file_size;
-    result_file.open("../../src/results/player_" + to_string(this->player) + ".result.json", ios::in);
+    result_file.open("src/results/player_" + to_string(this->player) + ".result.json", ios::in);
     if(!result_file){//result file does not exist
         cout << "Result file not found..." << endl;
-        result_file.close();
+        // result_file.close();
         return false;
     } else {//file exists, check size
         cout << "Result file found! Checking size..." << endl;
@@ -78,25 +99,32 @@ int Client::get_result() {
     ifstream result_file;
     char result;
     string line = "";
-    result_file.open("../../src/results/player_" + to_string(this->player) + ".result.json");
-    std::getline(result_file, line);
-    result_file.close();
-    for(int i = 0; i < line.size(); i++){//extract result
-        if (line[i] == ':'){
-            if(line[i + 1] == '1'){
-                return HIT;
-            } else if(line[i + 1] == '-' && line[i + 2] == '1') {
-                return MISS;
-            } else if(line[i + 1] == '0'){
-                return OUT_OF_BOUNDS;
-            } else if(line[i + 1] == ' ' || line[i + 1] == '\t'){
-                cout << "Error : whitespace detected as shot result" << endl;
+    result_file.open("src/results/player_" + to_string(this->player) + ".result.json");
+    
+    if(result_file){
+        std::getline(result_file, line);
+        result_file.close();
+        cleanFile("src/results/player_" + to_string(this->player) + ".result.json");
+        for(int i = 0; i < line.size(); i++){//extract result
+            if (line[i] == ':'){
+                if(line[i + 1] == '1'){
+                    return HIT;
+                } else if(line[i + 1] == '-' && line[i + 2] == '1') {
+                    return MISS;
+                } else if(line[i + 1] == '0'){
+                    return OUT_OF_BOUNDS;
+                } else if(line[i + 1] == ' ' || line[i + 1] == '\t'){
+                    cout << "Error : whitespace detected as shot result" << endl;
+                }
+                cout << "Error reading shot result" << endl;
             }
-            cout << "Error reading shot result" << endl;
         }
+        cout << "Error : file not in JSON format (i.e. a colon wasn't found)" << endl;
+        return OUT_OF_BOUNDS;
+    } else {
+        cout << "Error : Couldn't open result file, even though it exists" << endl;
+        return OUT_OF_BOUNDS;
     }
-    cout << "Error : file not in JSON format (i.e. a colon wasn't found)" << endl;
-    return OUT_OF_BOUNDS;
 }
 
 
@@ -122,5 +150,5 @@ void Client::update_action_board(int result, unsigned int x, unsigned int y) {
 
 
 string Client::render_action_board(){
-    return "this is the rendered action board";
+    return "this is the rendered action board \n";
 }
